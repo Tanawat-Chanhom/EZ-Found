0424f3f16328247fa622f7eb17445e8b2265a4f2
Tanawat <tanawatchanhom@MacBook-Pro-khxng-Tanawat.local>
2020-03-24 23:20:10 +0700

    Update front-end signup page css+html

--- ezFound/user/views.py
+++ ezFound/user/views.py
@@ -7,37 +7,37 @@ from django.contrib.auth.models import User
 def signup(request):
     # context = []
 
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        email = request.POST.get('email')
-        fname = request.POST.get('fname')
-        lname = request.POST.get('lname')
-        student_number = request.POST.get('student_number')
-        phonenumber = request.POST.get('phonenumber')
-        information = request.POST.get('information')
-        img = request.POST.get('img')
+    # if request.method == 'POST':
+    #     username = request.POST.get('username')
+    #     password = request.POST.get('password')
+    #     email = request.POST.get('email')
+    #     fname = request.POST.get('fname')
+    #     lname = request.POST.get('lname')
+    #     student_number = request.POST.get('student_number')
+    #     phonenumber = request.POST.get('phonenumber')
+    #     information = request.POST.get('information')
+    #     img = request.POST.get('img')
 
-        # if User.objects.filter(username=request.POST.get('username')):
-        #     context['error'] = 'Email has tacked!!'
-        #     return render(request, 'user/register.html', context=context)
+    #     # if User.objects.filter(username=request.POST.get('username')):
+    #     #     context['error'] = 'Email has tacked!!'
+    #     #     return render(request, 'user/register.html', context=context)
 
-        user = User.objects.create_user(
-            username=request.POST.get('username'),
-            password=request.POST.get('password')
-        )
+    #     user = User.objects.create_user(
+    #         username=request.POST.get('username'),
+    #         password=request.POST.get('password')
+    #     )
         
-        if user:
-            user_profile = Users(
-                user = user,
-                student_number = student_number,
-                fname = fname,
-                lname = lname,
-                phonenumber = phonenumber,
-                email = email,
-                information = information,
-                profile_img_path = img
-            )
-            user_profile.save()
-            return render(request, 'user/signup.html')
+    #     if user:
+    #         user_profile = Users(
+    #             user = user,
+    #             student_number = student_number,
+    #             fname = fname,
+    #             lname = lname,
+    #             phonenumber = phonenumber,
+    #             email = email,
+    #             information = information,
+    #             profile_img_path = img
+    #         )
+    #         user_profile.save()
+    #         return render(request, 'user/signup.html')
     return render(request, 'user/signup.html')
