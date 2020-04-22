61c326b578f30715ad30c6a6d37b958833181451
Tetat Chulchue <tetat.chulchue@gmail.com>
2020-04-23 00:22:27 +0700

    Update models of Post app

--- ezFound/account/models.py
+++ ezFound/account/models.py
@@ -2,17 +2,3 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-
-class UserProfile(models.Model):
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    student_number = models.IntegerField(null=False, unique=True)
-    phone_number = models.CharField(max_length=10)
-
-    information = models.TextField()
-    profile_img_path = models.TextField()
-
-    def __str__(self):
-        return self.user.username
-    
