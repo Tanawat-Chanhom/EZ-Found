8247a81ceeb7acd73bcb7a4485537490cdf53397
Tetat Chulchue <tetat.chulchue@gmail.com>
2020-04-26 21:06:33 +0700

    Update API
    + API now return category of post as array
    + Add create Post and get all post for index page

--- ezFound/api/urls.py
+++ ezFound/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 import api.views as api
 
 urlpatterns = [
-    # path('post'),
-    path('post/<int:postId>', api.post_get),  # Get specific post
-    path('post/user/<int:userId>', api.userPost),  # Get all post of specific user
+    path('post', api.post),                         # Get post for index page and create new post
+    path('post/<int:postId>', api.post_get),        # Get specific post
+    path('post/user/<int:userId>', api.userPost),   # Get all post of specific user
 ]
