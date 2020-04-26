e987c3d2201e1e70c793be65d8c30601303c5fe7
Tetat Chulchue <tetat.chulchue@gmail.com>
2020-04-26 21:07:18 +0700

    hotfix

--- ezFound/api/views.py
+++ ezFound/api/views.py
@@ -82,62 +82,62 @@ def userPost(request, userId):
 def post(request):
     """ Get post for index page and create new post """
 
-    # try:
-    if request.method == 'GET':
-        posts = Post.objects.all()
-        payload = [{
-            "id": p.id,
-            "title": p.title,
-            "description": p.descriptions,
-            "status": p.status,
-            "location": p.location.name,
-            "user": p.user.username,
-            "create_at": p.create_at,
-            "date": p.date
-        } for p in posts]
-
-        return JsonResponse({
-            "statusCode": 200,
-            "statusText": "Success",
-            "message": "Successt",
-            "error": False,
-            "data": payload
-        })
-
-    elif request.method == 'POST':
-        try:
-            post = Post(
-                title=request.data['title'],
-                descriptions=request.data['descriptions'],
-                status=request.data['status'],
-                date=request.data['date'],
-                user=User.objects.get(pk=request.data['user_id']),
-                location=Location.objects.get(pk=request.data['location_id'])
-            )
-            post.save()
-            for c in request.data['categories_id']:
-                post.category.add(Category.objects.get(pk=c))
-            post.save()
+    try:
+        if request.method == 'GET':
+            posts = Post.objects.all()
+            payload = [{
+                "id": p.id,
+                "title": p.title,
+                "description": p.descriptions,
+                "status": p.status,
+                "location": p.location.name,
+                "user": p.user.username,
+                "create_at": p.create_at,
+                "date": p.date
+            } for p in posts]
 
             return JsonResponse({
-                "statusCode": 201,
-                "statusText": "Created",
-                "message": "Create Post Successfully",
+                "statusCode": 200,
+                "statusText": "Success",
+                "message": "Successt",
                 "error": False,
+                "data": payload
             })
 
-        except KeyError:
-            return JsonResponse({
-                "statusCode": 400,
-                "statusText": "Bad Request",
-                "message": "Invalid API Payload",
-                "error": True
-        })
+        elif request.method == 'POST':
+            try:
+                post = Post(
+                    title=request.data['title'],
+                    descriptions=request.data['descriptions'],
+                    status=request.data['status'],
+                    date=request.data['date'],
+                    user=User.objects.get(pk=request.data['user_id']),
+                    location=Location.objects.get(pk=request.data['location_id'])
+                )
+                post.save()
+                for c in request.data['categories_id']:
+                    post.category.add(Category.objects.get(pk=c))
+                post.save()
 
-    # except:
-    #     return JsonResponse({
-    #         "statusCode": 500,
-    #         "statusText": "Internal Server",
-    #         "message": "Internal Server",
-    #         "error": True
-    #     })
+                return JsonResponse({
+                    "statusCode": 201,
+                    "statusText": "Created",
+                    "message": "Create Post Successfully",
+                    "error": False,
+                })
+
+            except KeyError:
+                return JsonResponse({
+                    "statusCode": 400,
+                    "statusText": "Bad Request",
+                    "message": "Invalid API Payload",
+                    "error": True
+            })
+
+    except:
+        return JsonResponse({
+            "statusCode": 500,
+            "statusText": "Internal Server",
+            "message": "Internal Server",
+            "error": True
+        })
